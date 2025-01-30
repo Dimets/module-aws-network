f21830d2bd34b2a91fee169d8ebd707042c24b48
Dimets <dimets1977@gmail.com>
2025-01-30 22:00:56 +0300

    style

--- main.tf
+++ main.tf
@@ -3,17 +3,17 @@ provider "aws" {
 }
 
 locals {
-  vpc_name = "${var.env_name} ${var.vpc_name}"
+  vpc_name     = "${var.env_name} ${var.vpc_name}"
   cluster_name = "${var.cluster_name}-${var.env_name}"
 }
 
 ## Определение AWS VPC
 resource "aws_vpc" "main" {
-    cidr_block = var.main_vpc_cidr
-    tags =  {
-        "Name"                                          = local.vpc_name,
-        "kubernetes.io/cluster/${local.cluster_name}"   = "shared",
-    }
+  cidr_block = var.main_vpc_cidr
+  tags = {
+    "Name"                                        = local.vpc_name,
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared",
+  }
 }
 
 # Определение подсети
